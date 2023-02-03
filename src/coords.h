4797d731f5a0c4d99b056aa1e2f5e371c08d4bde
Argyraspides <joshivinayak441@gmail.com>
2023-02-03 23:10:40 +1300

    Azimuth calculation now outputs in the full 0 to 360 degree range

--- src/coords.h
+++ src/coords.h
@@ -2,6 +2,7 @@
 #define coords_h
 #include <vector>
 #include <cmath>
+#define PI 3.14159265359
 
 // Equatorial Coordinates
 struct equat_coord
@@ -11,7 +12,7 @@ struct equat_coord
     float declination = 0.0f;
     float right_ascension_degrees = 0;
 
-    // Updates the right ascension in degrees 
+    // Updates the right ascension in degrees
     void update()
     {
         right_ascension_degrees =
@@ -43,22 +44,22 @@ horiz_coord equatToHoriz(
     float local_sidereal_time = GMST + gps_coo.longitude;
     float hour_angle = local_sidereal_time - eq_coo.right_ascension_degrees;
 
-    // Our altitude before correcting for elevation above sea level.
     float altitude =
         asin(
-            sin(eq_coo.declination) * sin(gps_coo.latitude) +
-            cos(eq_coo.declination) * cos(gps_coo.latitude) * cos(hour_angle));
+            sin(eq_coo.declination * PI / 180.0f) * sin(gps_coo.latitude * PI / 180.0f) +
+            cos(eq_coo.declination * PI / 180.0f) * cos(gps_coo.latitude * PI / 180.0f) * cos(hour_angle * PI / 180.0f));
 
-    // Our altitude before correcting for elevation above sea level.
     float azimuth =
-        asin(
-            -sin(hour_angle) * cos(eq_coo.declination) / cos(altitude));
+        atan2(sin(hour_angle * PI / 180.0f),
+              cos(eq_coo.declination * PI / 180.0f) * cos(gps_coo.latitude * PI / 180.0f) - sin(eq_coo.declination * PI / 180.0f) * sin(gps_coo.latitude * PI / 180.0f) * cos(hour_angle * PI / 180.0f));
+
+    azimuth = fmod((azimuth + 2 * PI), (2 * PI));
 
     // Our correction factor for elevation above sea level
     float local_hour_angle = GMST + gps_coo.longitude - eq_coo.right_ascension_degrees;
     float correction_factor =
-        1.0f / (cos(elevation) * cos(gps_coo.latitude) * cos(local_hour_angle) +
-                sin(gps_coo.latitude) * sin(eq_coo.declination));
+        1.0f / (cos(elevation) * cos(gps_coo.latitude * PI / 180.0f) * cos(local_hour_angle * PI / 180.0f) +
+                sin(gps_coo.latitude * PI / 180.0f) * sin(eq_coo.declination * PI / 180.0f));
 
     altitude *= correction_factor;
     azimuth *= correction_factor;
